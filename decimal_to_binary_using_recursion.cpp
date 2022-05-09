f49a5d4e740c28b37e8737cc47b80d7813ae7b35
AnkitVlekhak <102821100+AnkitVlekhak@users.noreply.github.com>
2022-05-09 19:39:49 +0530

    decimal to binary using recursion
    using recursion , we create a base condition. then we trust recursion and assume that binary will do my work for previous elements. and then we "cout"

---
+++ decimal_to_binary_using_recursion.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void binary(int n){
+    if(n==1){
+        cout<<1;
+        return;
+    }
+    binary(n/2);
+    cout<<(n%2);
+}
+
+int main() {
+    int n;cin>>n;
+    binary(n);
+	return 0;
+}
